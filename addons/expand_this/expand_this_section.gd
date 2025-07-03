f7348501c984d2f88cf818a51c45d509ebb1f5b1
malcolmcdixon <56974882+malcolmcdixon@users.noreply.github.com>
2025-07-03 20:14:08 +0100

    update data structure and add unfold method

--- addons/expand_this/expand_this_section.gd
+++ addons/expand_this/expand_this_section.gd
@@ -4,26 +4,44 @@ class_name ExpandThisSection
 ## ExpandThisSection
 ##
 ## Stores information about a parsed inspector section,
-## including the object type, category, group, and its control.
+## category, group, its control and parent control.
 ## Provides unique keys for group and category matching.
+
+#========== CONSTANTS ==========
 
 const KEY_SEPARATOR: String = "|"
 
-var object: String
+#========== MEMBER VARIABLES ==========
+
 var category: String
 var group: String
 var control: Control
+var parent: ExpandThisSection
 
-var group_key: String:
-	get:
-		return "%s%s%s%s%s" % [object, KEY_SEPARATOR, category, KEY_SEPARATOR, group]
-
-var category_key: String:
+var key: String:
 	get:
 		return "%s%s%s" % [category, KEY_SEPARATOR, group]
 
 
-func _init(_object: String, _category: String, _group: String) -> void:
-	object = _object
+#========== OVERRIDDEN VIRTUAL METHODS ==========
+
+func _init(_category: String = "", _group: String = "") -> void:
 	category = _category
 	group = _group
+
+
+#========== PUBLIC METHODS ==========
+
+func unfold() -> void:
+	var path: Array[Control] = []
+
+	var current := self
+	while current != null and current.control != null:
+		path.append(current.control)
+		current = current.parent
+
+	# traverse back down to unfold in order
+	path.reverse()
+	for ctrl in path:
+		if ctrl.has_method("unfold"):
+			ctrl.unfold()
